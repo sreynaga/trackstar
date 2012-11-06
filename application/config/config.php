c868b554846c4475b318df356c079c69501d4c7e
sreynaga <sergio.reynaga@gmail.com>
2012-11-05 21:08:37 -0700

    modifique la base_url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/trackstart';
+$config['base_url']	= 'http://sr-www.trackstart.com/';
 
 /*
 |--------------------------------------------------------------------------
