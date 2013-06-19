e0c463f0493c255cf9535c397c2396fd2bdf8421
Sergio Reynaga <sergio.reynaga@gmail.com>
2013-06-19 12:38:18 -0600

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'hsrybr08';
+$db['default']['password'] = 'password';
 $db['default']['database'] = 'trackstart';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
